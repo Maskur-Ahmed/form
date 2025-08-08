fee41bf7657e4e661bd161e1c27f85c9ffed9fad
Maskur Ahmed <maskurahmed392@gmail.com>
2025-08-08 12:17:53 +0600

    js var variable and some data type

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var maskur = "ahmed" ;
+var age = 21;
+var birth_year = 2004;
+var isSingle = true ;
+
+console.log(maskur);
+console.log(age);
+console.log(birth_year);
+console.log(isSingle);
+console.log(typeof maskur);
+console.log(typeof age);
+console.log(typeof birth_year);
+console.log(typeof isSingle);
---
+++ js/variable.js
@@ -0,0 +1,2 @@
+var name = "maskur" ;
+console.log(name)
