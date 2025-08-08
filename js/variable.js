07e979621d3bdb3fbaa0f6e99f81d33a074f6c88
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-08 15:46:29 +0600

    variable declairation

--- js/variable.js
+++ js/variable.js
@@ -1,2 +1,31 @@
-var name = "maskur" ;
-console.log(name)
+//! no keyword in variable name:
+// var false = 43;
+// var var = 49;
+
+// var true = 20;
+
+// var class = 40;
+
+//* 2. cannot start with a number like-:
+
+// var 34man = 'army';
+
+//! 3.cannot space in the name of variable like:-
+
+// var maskur ahmed = "a man";
+
+//* 4.variable are case sensetive like :-
+
+var maskur ="ahmed";
+var Maskur = "Ahmed";
+
+//* 5.no quotation on the name :-
+
+// ! like var "age" = 12;
+
+// * how to write a long variable name:
+
+var my_name_is = "Maskur"; //! snack case
+var myNameIs = "Maskur";  //!  camel case
+var MyNameIs = "Maskur";  //!  pascel case
+
