8a9c94e7c00502e728a64ab496aaaa7fea150a5d
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-09 15:23:51 +0600

    JS prseInt and parseFloat

---
+++ js/number.js
@@ -0,0 +1,22 @@
+var orrangePrice = 20;
+var applePrice = 30;
+
+console.log(orrangePrice + applePrice);
+
+var tShirtPrice = "30tk";
+var pantPrice = 20 ;
+
+var convertingNumber = parseInt(tShirtPrice)
+
+console.log(tShirtPrice);
+console.log(pantPrice,"tk");
+
+var fashion =(convertingNumber * pantPrice);
+
+var glassPrice =" 200.52465724565";
+var cGPrice = parseFloat(glassPrice);
+var total = (fashion + cGPrice.toFixed(2));
+
+var cTotal =parseFloat(total);
+console.log(cTotal);
+console.log(typeof(cTotal));
