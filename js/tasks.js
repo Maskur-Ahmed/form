517fb76133a6db9ea38c90e89d2e374fb95d30fa
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-09 23:04:42 +0600

    task 2 compelete

--- js/tasks.js
+++ js/tasks.js
@@ -13,3 +13,21 @@ console.log(back);
 
 //* task 1 complete;
 
+//! task 2 :
+
+var bangla = 80;
+var english = 83;
+var math = 89;
+var phisics = 90;
+var biology = 98;
+var numberOfSubject = 5;
+
+var calculationMarks = (bangla + english + math + phisics + biology);
+
+var avarageMarks = (calculationMarks / numberOfSubject);
+
+console.log(avarageMarks);
+
+//* task 2 complete;
+
+//! task 3 :
