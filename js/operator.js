48ae6ecd6ebe85039a40f64f4aa8ed25408f43a7
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-09 22:50:58 +0600

    concat explained

--- js/operator.js
+++ js/operator.js
@@ -10,6 +10,14 @@ alu *= 5;
 var cMoney = 1000;
 cMoney /= 5;
 // !concat:
+var man = "maskur";
+var weight = 52;
+console.log(man + weight);
+
+var age = 22;
+var realAge = "19";
+
+console.log(age + realAge);
 //google search :Number(x)
 //question:what is  isNaN()
 console.log(alu);
